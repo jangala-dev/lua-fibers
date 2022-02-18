48c186fe32ada255358da033a5bb33ca907d3bd9
Rich <rich@janga.la>
2022-02-18 02:30:14 +0000

    Ports sleep module

---
+++ fibers/sleep.lua
@@ -0,0 +1,74 @@
+-- Use of this source code is governed by the Apache 2.0 license; see COPYING.
+
+-- Timeout events.
+
+local op = require('fibers.op')
+local fiber = require('fibers.fiber')
+local now = fiber.now
+
+local Timeout = {}
+Timeout.__index = Timeout
+
+--- Creates a new operation that will sleep until a specific monotonic time.
+-- @param t monotonic time to sleep until
+-- @return a new operation that can be composed by the user
+local function sleep_until_op(t)
+   local function try()
+      return t <= now()
+   end
+   local function block(suspension, wrap_fn)
+      suspension.sched:schedule_at_time(t, suspension:complete_task(wrap_fn))
+   end
+   return op.new_base_op(nil, try, block)
+end
+
+--- Sleeps until a specific monotonic time.
+-- @param t monotonic time to sleep until
+local function sleep_until(t)
+   return sleep_until_op(t):perform()
+end
+
+--- Creates a new operation that will sleep for a specific number of seconds.
+-- @param t seconds to sleep for
+-- @return a new operation that can be composed by the user
+local function sleep_op(dt)
+   local function try() return dt <= 0 end
+   local function block(suspension, wrap_fn)
+      suspension.sched:schedule_after_sleep(dt, suspension:complete_task(wrap_fn))
+   end
+   return op.new_base_op(nil, try, block)
+end
+
+--- Sleeps for a specific number of seconds.
+-- @param t seconds to sleep for
+local function sleep(dt)
+   return sleep_op(dt):perform()
+end
+
+local function selftest()
+   print('selftest: lib.fibers.sleep')
+   local done = {}
+   local count = 1e3
+   for i=1,count do
+      local function fn()
+         local start, dt = now(), math.random()
+         sleep(dt)
+         assert(now() >= start + dt)
+         table.insert(done, i)
+      end
+      fiber.spawn(fn)
+   end
+   for t=now(),now()+1.5,0.01 do
+      fiber.current_scheduler:run(t)
+   end
+   assert(#done == count)
+   print('selftest: ok')
+end
+
+return {
+   sleep = sleep,
+   sleep_op = sleep_op,
+   sleep_until = sleep_until,
+   sleep_until_op = sleep_until_op,
+   selftest = selftest,
+}
