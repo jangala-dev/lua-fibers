dfcfc7de8e10e83a0092e7da11cb714d5b807c6e
Rich <rich@janga.la>
2023-03-17 22:15:26 +0000

    adds test file

---
+++ tests/test.lua
@@ -0,0 +1,34 @@
+package.path = "../?.lua;" .. package.path
+
+local package_name = 'stream'
+
+local modules = {
+    'stream',
+    'stream.file',
+    'stream.mem',
+    'stream.compat'
+}
+
+for _, j in ipairs(modules) do
+    require(package_name.."."..j).selftest()
+end
+
+local package_name = 'fibers'
+
+local modules = {
+    'timer', 
+    'sched', 
+    'fiber',
+    'channel',
+    'sleep',
+    'epoller',
+    'cond',
+    'file',
+    'queue',
+}
+
+for _, j in ipairs(modules) do
+    require(package_name.."."..j).selftest()
+end
+
+print('all tests passed!')
