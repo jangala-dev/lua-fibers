674bdc00bd275b160a209de3b07ca68cd1afacba
Rich <rich@janga.la>
2023-05-03 04:47:27 +0100

    Updates devcontainer to Ubuntu 22.04 and prunes packages installed

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,75 +4,31 @@ sudo apt update -y
 
 sudo apt install -y apt-utils unzip curl wget git build-essential libreadline-dev dialog libssl-dev m4
 
-cd /tmp
+# install core lua packages
 
-# install Go
+sudo apt install -y lua5.1 liblua5.1-dev luajit luarocks
 
-wget https://dl.google.com/go/go1.17.2.linux-arm64.tar.gz
-tar -xvf go1.17.2.linux-arm64.tar.gz
-mv go /usr/local
-rm go1.17.2.linux-arm64.tar.gz
-
-# install lua
-
-sudo apt install lua5.1
-sudo apt install liblua5.1-dev
-
-# install LuaRocks
-
-wget https://luarocks.org/releases/luarocks-3.7.0.tar.gz
-tar zxpf luarocks-*
-rm luarocks-3.7.0.tar.gz
-cd luarocks-*
-./configure --with-lua-include=/usr/local/include
-make
-sudo make install
-cd ..
-rm -rf luarocks-*
-
-sudo mkdir -p /usr/local/lib/luarocks/rocks-5.1
-
-# install luasocket
-
-sudo luarocks install luasocket
-
-# install nixio (the version on luarocks is broken - lovely)
-
-git clone https://github.com/jangala-dev/nixio
-cd nixio/
-sudo luarocks make
-
-cd ..
-
-# install lumen
-
-git clone https://github.com/xopxe/lumen
-sudo cp -r lumen /usr/local/lib/lua/*/
-rm -rf lumen
-
-# install cqueues
-
-sudo luarocks install cqueues
-
-# install luaposix
-
-sudo luarocks install luaposix
-
-# install bit32
+# install luarocks packages
 
 sudo luarocks install bit32
+sudo luarocks install cqueues
+sudo luarocks install http
+sudo luarocks install luaposix
 
 # install afghanistanyn/lua-epoll
-
+cd /tmp
+sudo rm -rf lua-epoll
 git clone https://github.com/afghanistanyn/lua-epoll
 cd lua-epoll/
-make
+gcc --shared -fPIC -o epoll.so epoll.c -I/usr/include/lua5.1
 sudo cp epoll.so /usr/local/lib/lua/5.1/epoll.so
 
 # install cffi-lua
 
-sudo apt install meson pkg-config cmake libffi-dev
+sudo apt install -y meson pkg-config cmake libffi-dev
 
+cd /tmp
+sudo rm -rf cffi-lua
 git clone https://github.com/q66/cffi-lua
 mkdir cffi-lua/build
 cd cffi-lua/build
