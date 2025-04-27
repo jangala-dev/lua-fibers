e3d1c627222586f78b4d692cc44e291704a2e6f3
Richard Thanki <richardthanki@gmail.com>
2025-04-27 22:43:57 +0000

    updates lua-http example, adds timeout capability

--- examples/2-lua-http/cq-http-fiber-test.lua
+++ examples/2-lua-http/cq-http-fiber-test.lua
@@ -20,30 +20,7 @@ print("installing stream based IO library")
 require 'fibers.stream.compat'.install()
 
 print("overriding cqueues step")
-local old_step; old_step = cqueues.interpose("step", function(self, timeout)
-	if cqueues.running() then
-		fiber.yield()
-		return old_step(self, timeout)
-	else
-		-- local t = self:timeout() or math.huge
-		-- if timeout then
-		-- 	t = math.min(t, timeout)
-		-- end
-        local events = self:events()
-        -- messy
-        if events == 'r' then
-            pollio.fd_readable_op(self:pollfd()):perform()
-        elseif events == 'w' then
-            pollio.fd_writable_op(self:pollfd()):perform()
-        elseif events == 'rw' then
-            op.choice(
-                pollio.fd_readable_op(self:pollfd()),
-                pollio.fd_writable_op(self:pollfd())
-            ):perform()
-        end
-		return old_step(self, 0.0)
-	end
-end)
+require "fibers_cqueues"
 
 local http_headers = require "http.headers"
 local http_server = require "http.server"
