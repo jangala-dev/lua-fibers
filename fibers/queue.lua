2fb92302bd222bbe71d0b43d8764d5a53e7992e8
Richard Thanki <richardthanki@gmail.com>
2025-06-12 08:01:57 +0000

    reimplements queues on directly buffered channels

--- fibers/queue.lua
+++ fibers/queue.lua
@@ -1,64 +1,17 @@
 -- Use of this source code is governed by the Apache 2.0 license; see COPYING.
 
 --- fibers.queue module
--- Provides Concurrent ML style buffered channels for communication between fibers.
+-- Buffered channels for communication between fibers.
 -- @module fibers.queue
 
-local op = require 'fibers.op'
 local channel = require 'fibers.channel'
-local fiber = require 'fibers.fiber'
-
---- Queue class
--- Represents a queue for communication between fibers.
--- @type Queue
--- local Queue = {}
 
 --- Create a new Queue.
 -- @int[opt] bound The upper bound for the number of items in the queue.
 -- @treturn Queue The created Queue.
 local function new(bound)
     if bound then assert(bound >= 1) end
-    local ch_in, ch_out = channel.new(), channel.new()
-    local function service_queue()
-        local q = {}
-        while true do
-            if #q == 0 then
-                -- Empty.
-                table.insert(q, ch_in:get())
-            elseif bound and #q >= bound then
-                -- Full.
-                ch_out:put(q[1])
-                table.remove(q, 1)
-            else
-                local getop = ch_in:get_op()
-                local putop = ch_out:put_op(q[1])
-                local val = op.choice(getop, putop):perform()
-                if val == nil then
-                    -- Put operation succeeded.
-                    table.remove(q, 1)
-                else
-                    -- Get operation succeeded.
-                    table.insert(q, val)
-                end
-            end
-        end
-    end
-    fiber.spawn(service_queue)
-    local ret = {}
-    function ret:put_op(x)
-        assert(x ~= nil)
-        return ch_in:put_op(x)
-    end
-
-    function ret:get_op()
-        return ch_out:get_op()
-    end
-
-    function ret:put(x) self:put_op(x):perform() end
-
-    function ret:get() return self:get_op():perform() end
-
-    return ret
+    return channel.new(bound and bound or math.huge)
 end
 
 return {
--- tests/test_queue.lua
+++ tests/test_queue.lua
@@ -14,16 +14,16 @@ local function record(x) table.insert(log, x) end
 
 fiber.spawn(function()
     local q = queue.new()
-    record('a');
-    q:put('b');
-    record('c');
-    q:put('d');
-    record('e');
+    record('a')
+    q:put('b')
+    record('c')
+    q:put('d')
+    record('e')
     record(q:get())
-    q:put('f');
-    record('g');
+    q:put('f')
+    record('g')
     record(q:get())
-    record('h');
+    record('h')
     record(q:get())
 end)
 
@@ -33,30 +33,8 @@ local function run(...)
     assert(equal(log, { ... }))
 end
 
--- 1. Main fiber runs, creating queue fiber.  It blocks trying to
--- hand off 'b' as the queue fiber hasn't run yet.
-run('a')
--- 2. Queue fiber runs, taking 'b', and thereby resuming the main
--- fiber (marking it runnable on the next turn).  Queue fiber blocks
--- trying to get or put.
-run()
--- 3. Main fiber runs, is able to put 'd' directly as the queue was
--- waiting on it, then blocks waiting for a 'get'.  Putting 'd'
--- resumed the queue fiber.
-run('c', 'e')
--- 4. Queue fiber takes 'd' and is also able to put 'a', resuming the
--- main fiber.
-run()
--- 5. Main fiber receives 'b', is able to put 'f' directly, blocks
--- getting from queue.
-run('b', 'g')
--- 6. Queue fiber resumed with 'f', puts 'd', then blocks.
-run()
--- 7. Main fiber resumed with 'd' and also succeeds getting 'f'.
-run('d', 'h', 'f')
--- 8. Queue resumes and blocks.
-run()
--- Nothing from here on out.
+-- With the new buffered channel implementation, the behavior is  direct:
+run('a', 'c', 'e', 'b', 'g', 'd', 'h', 'f')
 for _ = 1, 20 do run() end
 
 print('test: ok')
