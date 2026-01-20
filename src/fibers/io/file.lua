5ca93b65ec9d4bc3ba017852ba1aad1e7946f37b
Rich Thanki <richardthanki@gmail.com>
2026-01-20 00:48:27 +0000

    corrects some ineffective assertions

--- src/fibers/io/file.lua
+++ src/fibers/io/file.lua
@@ -69,7 +69,7 @@ end
 ---@return Stream
 local function fdopen(fd, flags_or_mode, filename)
 	-- assert(type(fd) == "number", "fdopen: fd must be a number")
-	assert(type(fd) ~= nil, 'fdopen: fd must be non-nil')
+	assert(fd ~= nil, 'fdopen: fd must be non-nil')
 
 	local readable, writable
 
